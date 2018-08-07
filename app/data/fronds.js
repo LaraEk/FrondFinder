7d1725fc6e5d70ce443d3b4ed631de4159664ce0
Ekscursionist <larakek@gmail.com>
2018-08-07 15:13:36 -0400

    making it work; modeled off in-class exercise #14

---
+++ app/data/fronds.js
@@ -0,0 +1,26 @@
+// ===============================================================================
+// DATA
+// ===============================================================================
+
+var frondArray = [
+    {
+        "name":"sedum sempervivum",
+        "photo":"https://i.imgur.com/nKOCAhnr.jpg",
+        "scores":[
+            5,
+            1,
+            5,
+            1,
+            1,
+            1,
+            5,
+            5,
+            1,
+            5
+          ]
+      }      
+  ];
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = frondArray;
+  
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,70 @@
+// ===============================================================================
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+// These data sources hold arrays of information on table-data, waitinglist, etc.
+// ===============================================================================
+
+var frondData = require("../data/fronds");
+// var waitListData = require("../data/waitinglistData");
+
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+  // API GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases when a user visits a link
+  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+  // ---------------------------------------------------------------------------
+
+  app.get("/api/fronds", function(req, res) {
+    res.json(fronds);
+  });
+
+//   app.get("/api/waitlist", function(req, res) {
+//     res.json(waitListData);
+//   });
+
+  // API POST Requests
+  // Below code handles when a user submits a form and thus submits data to the server.
+  // In each of the below cases, when a user submits form data (a JSON object)
+  // ...the JSON is pushed to the appropriate JavaScript array
+  // (ex. User fills out a reservation request... this data is then sent to the server...
+  // Then the server saves the data to the tableData array)
+  // ---------------------------------------------------------------------------
+
+  // NOTE: THIS WILL PUSH PLANT INFO TO FRONDS.  YOU NEED ANOTHER ONE TO PUT TO FRIENDS
+  app.post("/api/fronds", function(req, res) {
+    tableData.push(req.body);
+    res.json(true);
+    // // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+    // // It will do this by sending out the value "true" have a table
+    // // req.body is available since we're using the body-parser middleware
+    // if (tableData.length < 5) {
+    //   tableData.push(req.body);
+    //   res.json(true);
+    // }
+    // else {
+    //   waitListData.push(req.body);
+    //   res.json(false);
+    // }
+  });
+
+  // ---------------------------------------------------------------------------
+  // I added this below code so you could clear out the table while working with the functionality.
+  // Don"t worry about it!
+
+  // ---------------------------------------------------------------------------
+  // ------- COMMENTING THIS OUT BECAUSE I WANT TO RETAIN DATA ACROSS USES ------- //
+  // ---------------------------------------------------------------------------
+
+  //   app.post("/api/clear", function() {
+//     // Empty out the arrays of data
+//     tableData = [];
+//     waitListData = [];
+
+//     console.log(tableData);
+//   });
+};
