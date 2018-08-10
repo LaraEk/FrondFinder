043a0245e7657942e47c7bd65cfffa34d6152b2b
Ekscursionist <larakek@gmail.com>
2018-08-10 16:58:44 -0400

    populating friendsArray with some individuals

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,7 +4,7 @@
 
 var friendArray = [
     {
-        "name":"I like succulents",
+        "name":"Mr Eyebrows",
         "pic":"https://i.imgur.com/j8OrbWh.jpg",
         "scores":[
             5,
@@ -18,7 +18,87 @@ var friendArray = [
             1,
             5
           ]
-      }      
+      },
+      {
+        "name":"I Answered All 1s",
+        "pic":"https://i.imgur.com/RFUYT0u.jpg",
+        "scores":[
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+          ]
+      },
+      {
+        "name":"I Answered All 2s",
+        "pic":"https://i.imgur.com/pHW171G.jpg",
+        "scores":[
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2
+          ]
+      },
+      {
+        "name":"I Answered All 3s",
+        "pic":"https://i.imgur.com/WaQGkR3.jpg",
+        "scores":[
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3
+          ]
+      },
+      {
+        "name":"I Answered All 4s",
+        "pic":"https://i.imgur.com/AJYMrxt.jpg",
+        "scores":[
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4
+          ]
+      },
+      {
+        "name":"I Answered All 5s",
+        "pic":"https://i.imgur.com/GJWLCFU.jpg",
+        "scores":[
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5
+          ]
+      }
   ];
   
   // Note how we export the array. This makes it accessible to other files using require.
