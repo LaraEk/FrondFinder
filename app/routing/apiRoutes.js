cf16339a280e7ab7a5d8529d4cc109506a4c73b2
Ekscursionist <larakek@gmail.com>
2018-08-10 16:24:08 -0400

    trying to get the friends POST in apiRoutes to work, but it's being a complete putz

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,25 +1,24 @@
+// I am going to add Path just in case I need to require dependencies.
+var path = require("path");
+
 // ===============================================================================
 // LOAD DATA
 // We are linking our routes to a series of "data" sources.
 // These data sources hold arrays of information on table-data, waitinglist, etc.
 // ===============================================================================
-
 var frondData = require("../data/fronds");
 var friendData = require("../data/friends");
-// var waitListData = require("../data/waitinglistData");
 
 
 // ===============================================================================
 // ROUTING
 // ===============================================================================
-
 module.exports = function(app) {
   // API GET Requests
   // Below code handles when users "visit" a page.
   // In each of the below cases when a user visits a link
   // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
   // ---------------------------------------------------------------------------
-
   app.get("/api/fronds", function(req, res) {
     res.json(frondData);
   });
@@ -27,35 +26,62 @@ module.exports = function(app) {
   app.get("/api/friends", function(req, res) {
     res.json(friendData);
   });
-//   app.get("/api/waitlist", function(req, res) {
-//     res.json(waitListData);
-//   });
+
 
   // API POST Requests
   // Below code handles when a user submits a form and thus submits data to the server.
   // In each of the below cases, when a user submits form data (a JSON object)
   // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
   // ---------------------------------------------------------------------------
 
   // NOTE: THIS WILL PUSH FRIEND INFO TO FRIENDS.  YOU NEED ANOTHER ONE TO GET STUFF FROM FRONDSDB
   app.post("/api/friends", function(req, res) {
-    friendData.push(req.body);
-    res.json(true);
+    // friendData.push(req.body);
+    // res.json(true);
+
+    var newUserScores = req.body.scores;
+
+    var matchname = "";
+    var matchpic = "";
+    var totaldifference = 1000; // this is what we're comparing the difference to. not sure why so high.
+
+    for (var i = 0; i < friendData.length; i++) {
+
+      var difference = 0;
+      for (var z = 0; z < newUserScores.length; z++) {
+        difference += Math.abs(friendData[i].scores[z] - newUserScores[z]);
+      }
+
+      if (difference < totaldifference) {
+        console.log('Closest match found = ' + difference);
+				console.log('Friendname = ' + friendData[i].name);
+				console.log('Friendpic = ' + friendData[i].pic);
+
+        totaldifference = difference;
+        matchname = friendData[i].name;
+        matchpic = friendData[i].pic;
+      }
+    }
+
+  friendData.push(req.body);
+  res.json({status: "pushed", matchname: matchname, matchpic: matchpic});
+
   });
 
-    app.post("/api/fronds", function(req, res) {
-      // connection.query from mysql -- look for that
-  });
+};
 
-      // friendData.push(req.body);
-      // res.json(true);
 
-      // --------------------------------------------------
-    // I WANT TO MAKE "fronds" post from a database
-    // think about this after you get "friends" working
-    // --------------------------------------------------
+
+
+  //   app.post("/api/fronds", function(req, res) {
+  //     // connection.query from mysql -- look for that
+  //     // --------------------------------------------------
+  //   // I WANT TO MAKE "fronds" post from a database
+  //   // think about this after you get "friends" working
+  //   // --------------------------------------------------
+  // });
+
+
 
     // // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
     // // It will do this by sending out the value "true" have a table
@@ -84,4 +110,3 @@ module.exports = function(app) {
 
 //     console.log(tableData);
 //   });
-};
